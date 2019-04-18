0070c133032572a9337f3f6bdda89964941a3a53
CallMeSaltyF1sh <643342586@qq.com>
2019-04-19 01:30:19 +0800

    改进可视化样式&模块化JS代码

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,30 +1,52 @@
 function $(s){
     return document.querySelectorAll(s);
 }
-
 let lis = $('#list li');
+let size = 64;
+let Dots = [];
+let height,width;
+let canvas = document.createElement('canvas');
+let ctx = canvas.getContext('2d');
+let box = $('#box')[0];
+box.appendChild(canvas);
+let line;
 
+/**
+ * 实例化MusicVisualizer
+ */
+let mv = new MusicVisualizer({
+    size: size,
+    visualizer: draw
+});
+
+/**
+ * 获取曲目列表-播放
+ */
 for(let i=0,n=lis.length;i<n;i++){
     lis[i].onclick = function(){
         for(let j=0;j<n;j++){
             lis[j].className = '';
         }
         this.className = 'selected';
-        load('/media/'+this.title);
+        //mv.load('/media/'+this.title);
+        mv.play('/media/'+this.title);
     }
 }
 
+$('#volume')[0].onmousemove = function(){
+    mv.changeVolume(this.value/this.max);
+}
+
+/*
 let xhr = new XMLHttpRequest();
 let ac = new (window.AudioContext || window.webkitAudioContext)();
 let gainNode = ac[ac.createGain?'createGain':'createGainNode']();
 gainNode.connect(ac.destination);
 
 let analyser = ac.createAnalyser();
-let size = 128;
+
 analyser.fftSize = size * 2;
 analyser.connect(gainNode);
-
-let Dots = [];
 
 let source = null;
 let count = 0;
@@ -34,7 +56,7 @@ function load(url){
     if(count>1000){
         count = 0;
     }
-    source && source[source.stop ? 'stop' : 'noteOff']();
+    source && source[source.stop ? 'stop' : 'noteOff'](0);
     xhr.abort();
     xhr.open('get',url);
     xhr.responseType = 'arraybuffer';
@@ -59,10 +81,6 @@ function changeVolume(percent){
     gainNode.gain.value = percent * percent;
 }
 
-$('#volume')[0].onmousemove = function(){
-    changeVolume(this.value/this.max);
-}
-
 function visualizer(){
     let arr = new Uint8Array(analyser.frequencyBinCount);
     requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
@@ -75,14 +93,9 @@ function visualizer(){
     requestAnimationFrame(v);
 }
 visualizer();
+*/
 
-let height,width;
-let canvas = document.createElement('canvas')
-let ctx = canvas.getContext('2d');
-let box = $('#box')[0];
-box.appendChild(canvas);
 
-let line;
 function resize(){
     height = box.clientHeight;
     width = box.clientWidth;
@@ -98,33 +111,53 @@ function resize(){
 resize();
 window.onresize = resize;
 
+
+/**
+ * 画在canvas上
+ */
 function draw(arr){
     ctx.clearRect(0,0,width,height);
     let w = width/size;
+    let capW = w * 0.6;
+    let capH = capW > 6 ? 6 : capW;
     ctx.fillStyle = line;
 
     for(let i=0;i<size;i++){
+        let o = Dots[i];
         if(draw.type == 'column'){
             let h = arr[i]/256 * height;
-            ctx.fillRect(w*i, height-h,w*0.6,h);
+            ctx.fillRect(w*i, height-h,capW,h);
+            ctx.fillRect(w*i,height-o.cap-capH,capW,capH);
+            o.cap--;
+            if(o.cap<0){
+                o.cap = 0;
+            }
+            if(h > 0 && o.cap < h + 40){
+                o.cap = ((h + 40) > (height-capH)) ? (height-capH) : (h + 40);
+            }
         }else if(draw.type == 'dot'){
             ctx.beginPath();
-            var o = Dots[i];
-            var r = arr[i]/256 * 50;
+            
+            let r = 10 + arr[i]/256 * (height>width ? width : height)/10;
             ctx.arc(o.x,o.y,r,0,Math.PI*2,true);
             //ctx.strokeStyle = o.color;
             //ctx.stroke();
             let g = ctx.createRadialGradient(o.x,o.y,0,o.x,o.y,r);
-            g.addColorStop(0,'#fff');
+            g.addColorStop(0,o.color);
             g.addColorStop(1,o.color);
             ctx.fillStyle = g;
             ctx.fill();
+            o.x += o.dx;
+            o.x = o.x > width ? 0 : o.x;
+            o.y += o.dy;
+            o.y = o.y > height ? 0 : o.y;
         }
         
     }
 }
-draw.type = 'column';
 
+
+draw.type = 'column';
 /*顶部切换样式*/
 let types = $('#type li');
 for(let i=0,n=types.length;i<n;i++){
@@ -141,8 +174,6 @@ for(let i=0,n=types.length;i<n;i++){
 /**
  * 点状可视化
  */
-
-
 function random(m,n){
     return Math.round(Math.random()*(n-m)+m);
 }
@@ -151,11 +182,30 @@ function getDots(){
     for(let i=0;i<size;i++){
         let x = random(0,width);
         let y = random(0,height);
-        let color = 'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')';
+        let color = 'rgba('+random(0,255)+','+random(0,255)+','+random(0,255)+',0.6)';
         Dots.push({
             x: x,
             y: y,
-            color: color
+            dx: random(1,2),
+            dy: random(0,1),
+            color: color,
+            cap: 0   //柱状图上的小帽距离最低端的距离
         });
     }
 }
+
+/**
+ * 曲目上传
+ */
+/*
+$('#upload').onchange = function upload(){
+    let file = this.files[0];
+    let fr = new FileReader();
+
+    fr.onload = function(e){
+        mv.play(e.target.result);
+    }
+    fr.readAsArrayBuffer(file);
+    
+}
+*/
